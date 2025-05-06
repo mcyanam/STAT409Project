bc837f3e3918a94cf14b241dd2bf220e234b1a1a
Gabriel Pinochet-Soto <gpin2@pdx.edu>
2025-05-06 13:07:33 -0700

    Add more losses

--- Codes/src/dnnpype/loss.py
+++ Codes/src/dnnpype/loss.py
@@ -8,15 +8,21 @@ import jax.numpy as jnp
 import flax.nnx as nnx
 
 
-import jax.numpy as jnp
-
+###############################################################################
+# Formulae
+###############################################################################
 @jax.jit
 def _isingNumber(x_pipe: jnp.ndarray, theta: jnp.ndarray) -> jnp.ndarray:
     """Compute the (batched) Ising number."""
-    isBourdon, flueDepth, frequency, cutUpHeight, diameterToe, acousticIntensity = jnp.split(x_pipe, 6, axis=1)
+    isBourdon, flueDepth, frequency, cutUpHeight, diameterToe, acousticIntensity = (
+        jnp.split(x_pipe, 6, axis=1)
+    )
     pressure, density = theta
-    isingNumber = (1 / frequency) * jnp.sqrt((2 * pressure * flueDepth) / (density * jnp.power(cutUpHeight, 3)))
+    isingNumber = (1 / frequency) * jnp.sqrt(
+        (2 * pressure * flueDepth) / (density * jnp.power(cutUpHeight, 3))
+    )
     return isingNumber
+
 
 @jax.jit
 def _exponentialPartials(freq: jnp.ndarray, theta: jnp.ndarray) -> jnp.ndarray:
@@ -27,7 +33,28 @@ def _exponentialPartials(freq: jnp.ndarray, theta: jnp.ndarray) -> jnp.ndarray:
     partials = partials / jnp.max(partials, axis=1, keepdims=True)
     return partials
 
+@jax.jit
+def _linearPartials(freq: jnp.ndarray, theta: jnp.ndarray) -> jnp.ndarray:
+    """Compute the (batched) linear partials."""
+    n_part_from_freq = jnp.expand_dims(freq, axis=1) * jnp.arange(1, 9)
+    slope, intercept = jnp.split(theta, 2, axis=-1)
+    partials = n_part_from_freq * slope + intercept
+    partials = partials / jnp.max(partials, axis=1, keepdims=True)
+    return partials
 
+@jax.jit
+def _logPartials(freq: jnp.ndarray, theta: jnp.ndarray) -> jnp.ndarray:
+    """Compute the (batched) log partials."""
+    n_part_from_freq = jnp.expand_dims(freq, axis=1) * jnp.arange(1, 9)
+    slope, intercept = jnp.split(theta, 2, axis=-1)
+    partials = jnp.log(jnp.ones_like(n_part_from_freq) + n_part_from_freq * slope) + intercept
+    partials = partials / jnp.max(partials, axis=1, keepdims=True)
+    return partials
+
+
+################################################################################
+# Loss functions
+################################################################################
 def flatIsingLoss(
     model: nnx.Module,
     inputs: jnp.ndarray,
@@ -53,6 +80,7 @@ def flatIsingLoss(
     )
     return loss
 
+
 if __name__ == "__main__":
     # Test the loss function
     fakeData = jnp.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12]], dtype=jnp.float32)
@@ -60,3 +88,5 @@ if __name__ == "__main__":
     print(_isingNumber(fakeData, fakeTheta))
     fakeFreq = jnp.array([1, 2, 3, 4, 5, 6], dtype=jnp.float32)
     print(_exponentialPartials(fakeFreq, fakeTheta))
+    print(_linearPartials(fakeFreq, fakeTheta))
+    print(_logPartials(fakeFreq, fakeTheta))
