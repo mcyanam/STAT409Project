c11678da1f6f7e2b41a6a8beaf9dad324b9c442d
Gabriel Pinochet-Soto <gpin2@pdx.edu>
2025-05-08 11:34:18 -0700

    Correct losses

--- Codes/src/dnnpype/loss.py
+++ Codes/src/dnnpype/loss.py
@@ -10,7 +10,7 @@ import flax.nnx as nnx
 ###############################################################################
 # Formulae
 ###############################################################################
-@jax.jit
+@nnx.jit
 def _isingNumber(x_pipe: jnp.ndarray, theta: jnp.ndarray) -> jnp.ndarray:
     """Compute the (batched) Ising number."""
     _, flueDepth, frequency, cutUpHeight, _, _ = jnp.split(x_pipe, 6, axis=1)
@@ -21,31 +21,34 @@ def _isingNumber(x_pipe: jnp.ndarray, theta: jnp.ndarray) -> jnp.ndarray:
     return isingNumber
 
 
-@jax.jit
+@nnx.jit
 def _exponentialPartials(freq: jnp.ndarray, theta: jnp.ndarray) -> jnp.ndarray:
     """Compute the (batched) exponential partials."""
     n_part_from_freq = jnp.expand_dims(freq, axis=1) * jnp.arange(1, 9)
-    shift, intercept = jnp.split(theta, 2, axis=-1)
+    slicing_idx: list[int] = [1]
+    shift, intercept = jnp.split(theta, slicing_idx, axis=-1)
     partials = jnp.exp(-n_part_from_freq * intercept) + shift
     partials = partials / jnp.max(partials, axis=1, keepdims=True)
     return partials
 
 
-@jax.jit
+@nnx.jit
 def _linearPartials(freq: jnp.ndarray, theta: jnp.ndarray) -> jnp.ndarray:
     """Compute the (batched) linear partials."""
     n_part_from_freq = jnp.expand_dims(freq, axis=1) * jnp.arange(1, 9)
-    slope, intercept = jnp.split(theta, 2, axis=-1)
+    slicing_idx: list[int] = [1]
+    slope, intercept = jnp.split(theta, slicing_idx, axis=-1)
     partials = n_part_from_freq * slope + intercept
     partials = partials / jnp.max(partials, axis=1, keepdims=True)
     return partials
 
 
-@jax.jit
+@nnx.jit
 def _logPartials(freq: jnp.ndarray, theta: jnp.ndarray) -> jnp.ndarray:
     """Compute the (batched) log partials."""
     n_part_from_freq = jnp.expand_dims(freq, axis=1) * jnp.arange(1, 9)
-    slope, intercept = jnp.split(theta, 2, axis=-1)
+    slicing_idx: list[int] = [1]
+    slope, intercept = jnp.split(theta, slicing_idx, axis=-1)
     partials = (
         jnp.log(jnp.ones_like(n_part_from_freq) + n_part_from_freq * slope) + intercept
     )
@@ -60,15 +63,20 @@ def expLoss(
     model: nnx.Module,
     inputs: jnp.ndarray,
     theta: jnp.ndarray,
-    refValues: jnp.ndarray,
 ):
     """Flat partials + exact Ising number"""
-    # inputs: (isBourdon, flueDepth, frequency, cutUpHeight, diameterToe, acousticIntensity)
-    outputs = model(inputs)  # (ising, partial1, ..., partial8)
-    computedIsingNumber = _isingNumber(inputs, theta)
+    computedResults = model(inputs)  # (ising, partial1, ..., partial8)
+    slicing_idx: list[int] = [1]
+    computedIsingNumber, computedPartials = jnp.split(
+        computedResults, slicing_idx, axis=1
+    )
+    print(
+        f"{computedPartials.shape=}, {computedIsingNumber.shape=}, {computedResults.shape=}"
+    )
     _, _, frequency, _, _, _ = jnp.split(inputs, 6, axis=1)
-    computedPartials = _exponentialPartials(frequency, theta)
-    refIsingNumber, refPartials = jnp.split(refValues, 2, axis=1)
+    refIsingNumber = _isingNumber(inputs, theta)
+    refPartials = _exponentialPartials(frequency, theta)
+
     loss = jnp.mean(
         jnp.square(computedIsingNumber - refIsingNumber)
         + jnp.sum(jnp.square(computedPartials - refPartials), axis=1)
@@ -80,15 +88,17 @@ def linearLoss(
     model: nnx.Module,
     inputs: jnp.ndarray,
     theta: jnp.ndarray,
-    refValues: jnp.ndarray,
 ):
     """Linear partials + exact Ising number"""
-    # inputs: (isBourdon, flueDepth, frequency, cutUpHeight, diameterToe, acousticIntensity)
-    outputs = model(inputs)
-    computedIsingNumber = _isingNumber(inputs, theta)
+    computedResults = model(inputs)  # (ising, partial1, ..., partial8)
+    slicing_idx: list[int] = [1]
+    computedIsingNumber, computedPartials = jnp.split(
+        computedResults, slicing_idx, axis=1
+    )
     _, _, frequency, _, _, _ = jnp.split(inputs, 6, axis=1)
-    computedPartials = _linearPartials(frequency, theta)
-    refIsingNumber, refPartials = jnp.split(refValues, 2, axis=1)
+    refIsingNumber = _isingNumber(inputs, theta)
+    refPartials = _linearPartials(frequency, theta)
+
     loss = jnp.mean(
         jnp.square(computedIsingNumber - refIsingNumber)
         + jnp.sum(jnp.square(computedPartials - refPartials), axis=1)
@@ -100,15 +110,17 @@ def logLoss(
     model: nnx.Module,
     inputs: jnp.ndarray,
     theta: jnp.ndarray,
-    refValues: jnp.ndarray,
 ):
     """Log partials + exact Ising number"""
-    # inputs: (isBourdon, flueDepth, frequency, cutUpHeight, diameterToe, acousticIntensity)
-    outputs = model(inputs)
-    computedIsingNumber = _isingNumber(inputs, theta)
+    computedResults = model(inputs)  # (ising, partial1, ..., partial8)
+    slicing_idx: list[int] = [1]
+    computedIsingNumber, computedPartials = jnp.split(
+        computedResults, slicing_idx, axis=1
+    )
     _, _, frequency, _, _, _ = jnp.split(inputs, 6, axis=1)
-    computedPartials = _logPartials(frequency, theta)
-    refIsingNumber, refPartials = jnp.split(refValues, 2, axis=1)
+    refIsingNumber = _isingNumber(inputs, theta)
+    refPartials = _logPartials(frequency, theta)
+
     loss = jnp.mean(
         jnp.square(computedIsingNumber - refIsingNumber)
         + jnp.sum(jnp.square(computedPartials - refPartials), axis=1)
@@ -131,19 +143,12 @@ if __name__ == "__main__":
 
     # Test the loss function
     fakeModel = nnx.Linear(
-        6, 10, kernel_init=nnx.initializers.xavier_uniform(), rngs=nnx.Rngs(0)
+        6, 9, kernel_init=nnx.initializers.xavier_uniform(), rngs=nnx.Rngs(0)
     )
     fakeInputs = jnp.array(
         [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12]], dtype=jnp.float32
     )
     fakeTheta = jnp.array([1, 2], dtype=jnp.float32)
-    fakeRefValues = jnp.array([[1, 2], [3, 4]], dtype=jnp.float32)
-
-    loss = expLoss(fakeModel, fakeInputs, fakeTheta, fakeRefValues)
-    print("Loss (exp):", loss)
-
-    loss = linearLoss(fakeModel, fakeInputs, fakeTheta, fakeRefValues)
-    print("Loss (linear):", loss)
-
-    loss = logLoss(fakeModel, fakeInputs, fakeTheta, fakeRefValues)
-    print("Loss (log):", loss)
+    print(expLoss(fakeModel, fakeInputs, fakeTheta))
+    print(linearLoss(fakeModel, fakeInputs, fakeTheta))
+    print(logLoss(fakeModel, fakeInputs, fakeTheta))
